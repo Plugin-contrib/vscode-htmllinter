4aba392f44fd329a9ae2158c3c195845416d5e78
anikethsaha <anik220798@gmail.com>
2020-05-09 21:10:26 +0530

    build: added bundling

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const path = require('path')
+
+const config = {
+  target: 'node',
+  entry: './src/extension.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+  },
+}
+module.exports = config
